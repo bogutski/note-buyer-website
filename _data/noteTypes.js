54d50f386724eb11dce680e6c18b1553309540fb
Viktor Bogutskii <info@siteograf.com>
2023-10-11 01:01:01 -0400

    upd

---
+++ _data/noteTypes.js
@@ -0,0 +1,31 @@
+module.exports = [
+  {
+    title: 'Mortgage Note',
+    description: 'A mortgage note is a legal document that obligates a borrower to repay a loan at a stated interest rate during a specified period of time.',
+  },
+  {
+    title: 'Promissory Note',
+    description: 'A promissory note is a legal commitment where one party promises to pay a specific sum to another by a set date or upon demand.',
+  },
+  {
+    title: 'Deed of Trust',
+    description: 'A legal document that secures a loan by using real estate as collateral, typically involving three parties: borrower, lender, and trustee. It grants the trustee the power to sell the property if the borrower defaults on the loan.',
+  },
+  {
+    title: 'Contract for Deed',
+    description: 'An agreement where a buyer pays off property in installments and gains ownership only after the full payment is completed. Until then, the seller retains the title.',
+  },
+  {
+    title: 'Land Contract',
+    description: 'A contract between a buyer and seller of real property in which the seller provides financing to buy the property for an agreed-upon purchase price and the buyer repays the loan in installments.',
+  },
+  {
+    title: 'Real Estate Contract',
+    description: 'A Real Estate Contract is a binding agreement between a buyer and seller outlining the terms of a property sale. Ownership is transferred once stipulations are met.',
+  },
+  {
+    title: 'Bond for Deed',
+    description: 'A bond for deed is a type of real estate sale where a buyer makes a series of payments to the seller, and when the payments are completed, the seller transfers the deed to the property.',
+  }
+
+];
