2a5aed11f0fa206aa4250546f0b7200ebe8af656
Andrew1326 <maxgreen007007@gmail.com>
2023-10-31 11:50:08 +0000

    purgecss plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
+const purgeCssPlugin = require('eleventy-plugin-purgecss');
+
 module.exports = function (eleventyConfig) {
   // Копирует содержимое assets в выходной каталог
   eleventyConfig.addPassthroughCopy('assets');
+
+  eleventyConfig.addPlugin(purgeCssPlugin, {
+    confirm: './purgecss.config.js',
+    quiet: false,
+  })
 };
---
+++ purgecss.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    content: ['./_site/**/*.html', './_site/**/*.js'], // Content files referencing CSS classes
+    css: ["./_site/**/*.css"], // CSS files to be purged in-place
+}
